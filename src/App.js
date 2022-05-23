4a5ecd8e8005f0d3b00125b1e99779636304b307
Justin Fowler <justin.fowler@hey.com>
2022-05-23 16:21:13 +0000

    Adding compontent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// <img src={logo} className="App-logo" alt="logo" />
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tenzies</h1>
+      <p>Roll the dice a bunch of times!</p>
+      <Dice />
     </div>
   );
 }
---
+++ src/Dice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Dice() {
+	return (
+		<h1>Imma dice</h1>
+	)
+}
+
+export default Dice;
